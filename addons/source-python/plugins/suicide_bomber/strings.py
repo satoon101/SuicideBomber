52a573d1a584efbe3a7c925b7a29c9ca97a68adb
satoon101 <satoon101@gmail.com>
2025-06-19 18:39:53 -0400

    Minor changes.

--- addons/source-python/plugins/suicide_bomber/strings.py
+++ addons/source-python/plugins/suicide_bomber/strings.py
@@ -1,6 +1,6 @@
 # ../death_beam/strings.py
 
-"""Contains all translation variables for the base plugin."""
+"""Contains all translation variables for the plugin."""
 
 # =============================================================================
 # >> IMPORTS
